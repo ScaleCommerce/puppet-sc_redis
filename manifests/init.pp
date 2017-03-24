64c5a0c0c410d1f09709a554b9d326ec51f01179
Thomas Lohner <tl@syseleven.de>
2017-03-24 10:06:16 +0100

    get supervisor params from sc_supervisor-class

--- manifests/init.pp
+++ manifests/init.pp
@@ -24,29 +24,23 @@
 #
 
 class sc_redis (
-  $supervisor_init_script = '/etc/supervisor.init/supervisor-init-wrapper',
-  $supervisor_conf_script = '/etc/supervisor.d/redis.conf',
-  $supervisor_exec_path   = '/usr/local/bin',
 ) {
 
+  include sc_supervisor
   include redis
 
   # supervisor
   file { '/etc/init.d/redis-server':
     ensure => link,
-    target => $supervisor_init_script,
+    target => "${sc_supervisor::init_path}/supervisor-init-wrapper",
   }
 
-  file { $supervisor_conf_script:
+  file { "${supervisord::config_include}/redis-server.conf:
     owner   => 'root',
     group   => 'root',
     mode    => '0644',
     content => template("${module_name}/redis-server.supervisor.conf.erb"),
-    notify  => Exec['supervisorctl_redis_update'],
+    notify  => Class[supervisord::reload],
   }
 
-  exec {'supervisorctl_redis_update':
-    command     => "${supervisor_exec_path}/supervisorctl update",
-    refreshonly => true,
-  }
 }
